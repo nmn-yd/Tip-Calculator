f9afe1123660b563aa728b84afda80b671261dba
Naman Yadav <raymannky2001@gmail.com>
2023-07-31 22:26:38 +0530

    final version done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 import { useState } from "react";
 
 export default function App() {
-  const [bill, setbill] = useState(0);
+  return <Tipcalculator />;
+}
+
+function Tipcalculator() {
+  const [bill, setbill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+
+  const finalAmount = bill * ((percentage1 + percentage2) / 2 / 100);
 
   function totalbill(amount) {
     setbill(amount);
   }
 
-  console.log(bill);
+  function reset() {
+    setbill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
 
   return (
     <section className="head-container">
-      <Bill totalbill={totalbill} />
-      <Service bill={bill} totalbill={totalbill}>
+      <Bill bill={bill} totalbill={totalbill} />
+      <Service val={percentage1} per={setPercentage1}>
         How did you like the service?
       </Service>
-      <Service bill={bill} totalbill={totalbill}>
+      <Service val={percentage2} per={setPercentage2}>
         How did your friend like the service?
       </Service>
-      <Result bill={bill} />
+      <Result reset={reset} bill={bill} finalAmount={finalAmount} />
     </section>
   );
 }
 
-function Bill({ totalbill }) {
+function Bill({ bill, totalbill }) {
   return (
     <div>
       <p>How much was the bill?</p>
@@ -31,25 +43,26 @@ function Bill({ totalbill }) {
         className="input-field"
         type="number"
         id="num"
-        onChange={(el) => totalbill(Number(el.target.value))}
+        value={bill}
+        onChange={(el) => {
+          return totalbill(Number(el.target.value));
+        }}
       />
     </div>
   );
 }
 
-function Service({ children, bill, totalbill }) {
+function Service({ children, per, val }) {
   return (
     <div>
       <p>{children}</p>
       <select
         className="input-field"
+        value={val}
         onChange={(el) => {
-          const tip = el.target.value / 2;
-          const amount = tip + bill;
-          return totalbill(Number(amount));
+          return per(Number(el.target.value));
         }}
       >
-        <option value="0">--choose options--</option>
         <option value="0">Dissatisfied (0%)</option>
         <option value="5">It was okay (5%)</option>
         <option value="10">It was good (10%)</option>
@@ -58,11 +71,19 @@ function Service({ children, bill, totalbill }) {
     </div>
   );
 }
-function Result({ bill }) {
+function Result({ reset, bill, finalAmount }) {
   return (
     <div>
-      {bill ? <h1>{bill}</h1> : null}
-      <button>Reset</button>
+      {bill > 0 ? (
+        <div>
+          <h1>
+            You Pay RS {(bill + finalAmount).toFixed(2)} <br /> (RS {bill} + RS{" "}
+            {finalAmount.toFixed(2)} Tip)
+          </h1>
+        </div>
+      ) : null}
+
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
